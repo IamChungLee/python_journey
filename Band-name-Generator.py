ae654ceff8d3dee95f5397d3dfb43408df3d86ed
Chung Lee <chunglee@Chungs-MacBook-Pro.local>
2021-04-18 15:44:39 -0400

    Add python script

---
+++ Band-name-Generator.py
@@ -0,0 +1,8 @@
+
+print("Hi Ready for the Bandname experience of your life?")
+
+city = input("What city were you matured in?\n")
+
+petname = input("What was the name of your animal companion?\n")
+
+print("Your band name could be " + city + " " + petname)
